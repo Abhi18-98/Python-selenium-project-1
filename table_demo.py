219d1b2b54e5cc145b47ff6cc6d474a8a351ba77
Abhishek <abhishek.g5551@gmail.com>
2025-04-12 17:02:43 +0530

    to find rows in table

---
+++ table_demo.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+import time
+driver=webdriver.Chrome()
+
+driver.get("https://demo.guru99.com/test/table.html")
+rows=driver.find_elements(By.TAG_NAME,"tr")
+print ("Total rows:", len(rows))
+for row in rows:
+    cells=row.find_elements(By.TAG_NAME, "td")
+    for cell in cells:
+        print(cell.text, end=" | ")
+    print()  
+time.sleep(10)     
+driver.quit()     
